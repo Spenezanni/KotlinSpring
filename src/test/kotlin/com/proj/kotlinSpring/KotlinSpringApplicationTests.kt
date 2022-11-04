fdc07345bfa0127536bf8b443444cc3fef97ed28
Spenezanni <gustavospenezanni@gmail.com>
2022-11-04 07:56:41 -0300

    Initializing project blg

---
+++ src/test/kotlin/com/proj/kotlinSpring/KotlinSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.proj.kotlinSpring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
